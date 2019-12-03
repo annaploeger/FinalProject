38142b18596375ce34a71022782faf7d96c65c78
annaploeger <54840568+annaploeger@users.noreply.github.com>
2019-12-03 22:01:43 +0200

    Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page */ 
+/* GET home page */
+
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Microblog" });
-}); 
+  res.render("index", { title: "Mibo." });
+});
 
 router.get("/login", function(req, res, next) {
-  res.render("login", { title: "Login" });
+  res.render("login", { title: "Login." });
 });
 
 router.get("/register", function(req, res, next) {
-  res.render("register", { title: "Sign Up" });
+  res.render("register", { title: "Sign Up." });
 });
 
 router.get("/profile", function(req, res, next) {
-  res.render("profile", { title: "Your Profile" });
+  res.render("profile", { title: "Your Profile." });
 });
 
 module.exports = router;
